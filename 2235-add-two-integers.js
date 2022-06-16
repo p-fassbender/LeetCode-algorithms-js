2026cdf4d22a06ab0883b36b6aeaafb72e0744b3
Preston Fassbender <fassbenderp0551@gmail.com>
2022-06-16 18:02:57 -0500

    return the sum of two numbers

---
+++ 2235-add-two-integers.js
@@ -0,0 +1,11 @@
+/*
+Given two integers num1 and num2, return the sum of the two integers.
+*/
+
+var sum = function(num1, num2) {
+    // console.log(num1+num2)
+    return num1 + num2
+};
+
+sum(12, 5); // should return 17
+sum(-10, 4) // should return -6
