5059de1c1fa8c4576353d18005d77dca53459a70
Preston Fassbender <fassbenderp0551@gmail.com>
2024-05-29 14:47:37 -0500

    number is a power of two algorithm alternative solution

--- power-of-two.js
+++ power-of-two.js
@@ -13,10 +13,22 @@ function isPowerOfTwo(n) {
         if (n % 2 != 0) return false
         n = n / 2;
     }
-    
+
     return true;
 }
 
 console.log(isPowerOfTwo(1)) // true (2^0)
 console.log(isPowerOfTwo(2)) // true (2^1)
 console.log(isPowerOfTwo(5)) // false
+
+
+
+//time complexity = O(1)
+function isPowerOfTwoBitwise(n) {
+    if (n < 1) return false;    
+    return (n & (n-1)) === 0;
+}
+
+console.log(isPowerOfTwoBitwise(1)) // true (2^0)
+console.log(isPowerOfTwoBitwise(2)) // true (2^1)
+console.log(isPowerOfTwoBitwise(5)) // false
