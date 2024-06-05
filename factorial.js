e7af7fe5dd42566f0403188fc1101d8404c20bd4
Preston Fassbender <fassbenderp0551@gmail.com>
2024-06-05 15:07:00 -0500

    factorial algorithm recursive solution

--- factorial.js
+++ factorial.js
@@ -1,13 +1,11 @@
-//problem: given an integer 'n', fiond the factorial of thjat integer
-
+// PROBLEM: given an integer 'n', fiond the factorial of that integer
 // in math, the factorial of a non-negative integer 'n', denoted n!, is the product of all positive integers less than or equal to 'n'
-// facotrial of zero is 1
-
+// factorial of zero is 1
 // example: factorial(4) = 4*3*2*1 = 24
 // example: factorial(5) = 5*4*3*2*1 = 120
 
+// count down from n
 function factorial(n) {
-    if (!n) return 1
     let result = 1;
     for (let i = n; i > 0; i--) {
         result *= i
@@ -20,8 +18,7 @@ console.log(factorial(1)) // 1
 console.log(factorial(4)) // 4*3*2*1 = 24
 console.log(factorial(5)) // 5*4*3*2*1 = 120
 
-
-
+// count up to n
 function factorialAgain(n) {
     let result = 1;
     for (let i = 2; i <= n; i++) {
@@ -34,3 +31,21 @@ console.log(factorialAgain(0)) // 1
 console.log(factorialAgain(1)) // 1
 console.log(factorialAgain(4)) // 4*3*2*1 = 24
 console.log(factorialAgain(5)) // 5*4*3*2*1 = 120
+
+
+
+// PROBLEM: Given an integer 'n', find the factorial of that integer
+// The factorial of a non-negative integer 'n', denoted n!, is the product of all positive inegers less than or equal to 'n'
+// Factorial of zero is 1
+// example: recursiveFactorial(4) = 4*3*2*1 = 24
+// example: recursiveFactorial(5) = 5*4*3*2*1 = 120
+
+// time complexity: O(n)
+function recursiveFactorial(n) {
+    if (n === 0) return 1
+    return n * recursiveFactorial(n - 1)
+}
+console.log(recursiveFactorial(0)) // 1
+console.log(recursiveFactorial(1)) // 1
+console.log(recursiveFactorial(4)) // 4*3*2*1 = 24
+console.log(recursiveFactorial(5)) // 5*4*3*2*1 = 120
