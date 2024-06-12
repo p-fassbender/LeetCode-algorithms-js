64eb252e9053a98aab5c1fdc3b7273c7deeea3b1
Preston Fassbender <fassbenderp0551@gmail.com>
2024-06-12 12:02:56 -0500

    binary search recursive algorithm solution

--- binary-search.js
+++ binary-search.js
@@ -26,3 +26,35 @@ function binarySearch(arr, target) {
 console.log(binarySearch([-5, 2, 4, 6, 10], 10)) // should return 4
 console.log(binarySearch([-5, 2, 4, 6, 10], 6)) // should return 3
 console.log(binarySearch([-5, 2, 4, 6, 10], 20)) // should return -1
+
+
+
+// PROBLEM: given a sorted array of 'n' elements and a target element 't', find the index of 't' in the array. Return -1 if the target element is not found
+// example: arr=[-5, 2, 4, 6, 10], t=10 -> should return 4
+// example: arr=[-5, 2, 4, 6, 10], t=6 -> should return 3
+// example: arr=[-5, 2, 4, 6, 10], t=20 -> should return -1
+
+// time complexity: O(logn)
+function recursiveBinarySearch(arr, target) {
+    return search(arr, target, 0, arr.length - 1)
+}
+
+function search(arr, target, leftIndex, rightIndex) {
+    if (leftIndex > rightIndex) {
+        return -1
+    }
+    let middleIndex = Math.floor((leftIndex + rightIndex) / 2)
+    if (target === arr[middleIndex]) {
+        return middleIndex
+    }
+    if (target < arr[middleIndex]) {
+        return search(arr, target, leftIndex, middleIndex)
+    }
+    else {
+        return search(arr, target, middleIndex + 1, rightIndex)
+    }
+}
+
+console.log(recursiveBinarySearch([-5, 2, 4, 6, 10], 10)) // should return 4
+console.log(recursiveBinarySearch([-5, 2, 4, 6, 10], 6)) // should return 3
+console.log(recursiveBinarySearch([-5, 2, 4, 6, 10], 20)) // should return -1
