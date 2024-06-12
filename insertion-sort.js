3e338415093405bcbf2d782f1b79a01e10787a0b
Preston Fassbender <fassbenderp0551@gmail.com>
2024-06-12 12:59:53 -0500

    insertion sort algorithm solution

---
+++ insertion-sort.js
@@ -0,0 +1,19 @@
+// PROBLEM: given an array of integers, sort the array
+// const arr = [-6, 20, 8, -2, 4]
+// insertionSort(arr) => should return [-6, -2, 4, 8, 20]
+
+function insertionSort(arr) {
+    for (let i = 1; i < arr.length; i++) {
+        let numberToInsert = arr[i]
+        let j = i - 1
+        while (j >= 0 && arr[j] > numberToInsert) {
+            arr[j + 1] = arr[j]
+            j = j - 1
+        }
+        arr[j + 1] = numberToInsert
+    }
+}
+
+const arr = [-6, 20, 8, -2, 4]
+insertionSort(arr)
+console.log(arr) // should return [-6, -2, 4, 8, 20]
